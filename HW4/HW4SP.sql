0ad286222112ce7745ebd3723663f394323b3483
Shahid Foy <shahidfoy@gmail.com>
2017-10-18 19:48:30 -0600

    Update HW4SP.sql

--- HW4/HW4SP.sql
+++ HW4/HW4SP.sql
@@ -1,4 +1,9 @@
 /*
+Mohammad Shahid Foy
+Homework 4
+CS 3810
+Mrs. Cohen
+
 B) Write a PL/pgSQL stored procedure that analyzes the various flights of the airlines. The report should display:
 Airline name, total number of flights, average distance, ratio of passengers to distance, ratio of freight to
 distance, correlation coefficient for passengers to freight, correlation coefficient for passengers to distance.
