31ade48911e8128f836f763081b4e770d4be4e10
Shahid Foy <shahidfoy@gmail.com>
2017-11-04 16:49:17 -0600

    Add files via upload

---
+++ HW6/HW6Load.sql
@@ -0,0 +1,3 @@
+INSERT INTO Airline (Airline_name, Airline_country) VALUES ('UNITED', 'USA');
+INSERT INTO Airline (Airline_name, Airline_country) VALUES ('BRITISH', 'UK');
+INSERT INTO Airline (Airline_name, Airline_country) VALUES ('AIRCAN', 'CAN');
